588393f2c111adeb8dbbd50085a495a9b96f304c
Mauricio Degregori <degregori2@gmail.com>
2020-04-28 18:24:11 -0500

    more stuff added on test.py

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/home/<string:name>')
-def hello_name(name):
-    return f"Hello, {name}"
+@app.route("/")
+def hello():
+    return "<h1>Home Page!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page!</h1>"
+
+#debugger on
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
