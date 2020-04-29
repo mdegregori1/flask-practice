39d24a4fd2b3e765275cac54954e8370557c1593
Mauricio Degregori <degregori2@gmail.com>
2020-04-29 09:25:22 -0500

    added error handling

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 # secret key
@@ -11,5 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # pass app into sql db
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from test import routes
--- test/routes.py
+++ test/routes.py
@@ -1,8 +1,12 @@
 from test.models import User
 from test import app, db, bcrypt
 import json
-from flask import request, jsonify
+from flask import request, jsonify, Flask, make_response
+from flask_login import login_user
+import jwt
+import datetime
 
+app.config['SECRET_KEY'] = 'd6b5e117f8fc98bd1e97831d0bea530c'
 
 @app.route("/")
 def hello():
@@ -17,10 +21,11 @@ def about():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     req = request.json
+    auth = request.authorization
     user = User.query.filter_by(username=req["username"]).first()
     # user = User(username=form.username.data, password=hashed_password)
     if user:
-        return "Username already exists"
+        return make_response("Username already exists. Try again.", 400)
     
     entered_password_1 = req["password1"]
     entered_password_2 = req["password2"]
@@ -28,9 +33,11 @@ def register():
         new_user = User(username = req["username"],password = bcrypt.generate_password_hash(entered_password_1))
         db.session.add(new_user)
         db.session.commit()
-        return f"Created {new_user}"
+        token = jwt.encode({'id': new_user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode("ascii")})
+        
     else:
-        return "Passwords don't match. Try again."
+        return make_response("Sorry, passwords must match", 400)
     # hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     # db.session.add(user)
     # db.session.commit()
@@ -38,5 +45,13 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return "string for now"
+    req = request.json
+    auth = request.authorization
+    user = User.query.filter_by(username=req["username"]).first()
+    if user and bcrypt.check_password_hash(user.password, req["password"]):
+        token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode("ascii")})
+        # print(f"{user} logged in!")
+    else:
+        return make_response("Sorry, invalid credentials", 401)
  
